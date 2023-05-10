a575287c271a2545b295712d96d875ee81d5856d
葛绍飞 <geshaofei@gsf.local>
2023-05-10 21:40:33 +0800

    update

--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -2,8 +2,18 @@ const TypingAnimation = () => {
   return (
     <div className="flex items-center space-x-2">
       <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse"></div>
-      <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse delay-75"></div>
-      <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse delay-150"></div>
+      <div
+        className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse "
+        style={{ animationDelay: "200ms" }}
+      ></div>
+      <div
+        className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600  animate-pulse "
+        style={{ animationDelay: "500ms" }}
+      ></div>
+      <div
+        className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600  animate-pulse delay-500 "
+        style={{ animationDelay: "1s" }}
+      ></div>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,94 @@
 import { useState, useRef } from "react";
-import { Inter } from 'next/font/google'
-import axios from 'axios';
+import { Inter } from "next/font/google";
+import axios from "axios";
 import TypingAnimation from "../components/TypingAnimation";
 import Dialog from "@/components/Dialog";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const c = useRef()
-  const [inputValue, setInputValue] = useState('');
+  const c = useRef();
+  const [inputValue, setInputValue] = useState("");
   const [chatLog, setChatLog] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    setChatLog((prevChatLog) => [...prevChatLog, { type: 'user', message: inputValue }])
+    setChatLog((prevChatLog) => [
+      ...prevChatLog,
+      { type: "user", message: inputValue },
+    ]);
 
     sendMessage(inputValue);
 
-    setInputValue('');
-  }
-
+    setInputValue("");
+  };
 
   const sendMessage = (message) => {
-    const url = '/api/chat';
+    const url = "/api/chat";
 
     const data = {
       model: "gpt-3.5-turbo-0301",
-      messages: [{ "role": "user", "content": message }]
+      messages: [{ role: "user", content: message }],
     };
 
     setIsLoading(true);
-    setTimeout(() => c.current.scrollIntoView(false), 20)
-    axios.post(url, data).then((response) => {
-      setChatLog((prevChatLog) => [...prevChatLog, { type: 'bot', message: response.data.choices[0].message.content }])
-      setIsLoading(false);
-      setTimeout(() => c.current.scrollIntoView(false), 20)
-    }).catch((error) => {
-      setIsLoading(false);
-      console.log(error);
-    })
-  }
+    setTimeout(() => c.current.scrollIntoView(false), 20);
+    axios
+      .post(url, data)
+      .then((response) => {
+        setChatLog((prevChatLog) => [
+          ...prevChatLog,
+          { type: "bot", message: response.data.choices[0].message.content },
+        ]);
+        setIsLoading(false);
+        setTimeout(() => c.current.scrollIntoView(false), 20);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+      });
+  };
 
   return (
     <div className="container mx-auto max-w-[700px]">
       <div className="flex flex-col h-screen bg-gray-900">
-        <h1 className="bg-gradient-to-r from-green-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">ChatGPT</h1>
+        <h1 className="bg-gradient-to-r from-green-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">
+          ChatGPT
+        </h1>
         <div className="flex-grow p-6 overflow-auto">
-          <div ref={c} className="flex flex-col space-y-4">
-            {
-              chatLog.map((message, index) => (
-                <Dialog key={index} message={message} />
-              ))
-            }
-            {
-              isLoading &&
+          <div ref={c} className="flex flex-col space-y-2">
+            {chatLog.map((message, index) => (
+              <Dialog key={index} message={message} />
+            ))}
+            {!isLoading && (
               <div key={chatLog.length} className="flex justify-start">
                 <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
                   <TypingAnimation />
                 </div>
               </div>
-            }
+            )}
           </div>
         </div>
         <form onSubmit={handleSubmit} className="flex-none p-6">
           <div className="flex rounded-lg border border-gray-700 bg-gray-800">
-            <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-            <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
+            <input
+              type="text"
+              className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none"
+              placeholder="Type your message..."
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300"
+            >
+              Send
+            </button>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
