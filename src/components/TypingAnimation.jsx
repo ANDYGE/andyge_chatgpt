fdb0f59cec592851895e0ceceaa7ad102a5bbd9b
葛绍飞 <geshaofei@gsf.local>
2023-05-10 21:47:19 +0800

    添加了animation-delay

--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -1,19 +1,10 @@
 const TypingAnimation = () => {
   return (
     <div className="flex items-center space-x-2">
-      <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse"></div>
-      <div
-        className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse "
-        style={{ animationDelay: "200ms" }}
-      ></div>
-      <div
-        className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600  animate-pulse "
-        style={{ animationDelay: "500ms" }}
-      ></div>
-      <div
-        className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600  animate-pulse delay-500 "
-        style={{ animationDelay: "1s" }}
-      ></div>
+      <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse" />
+      <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse animation-delay-300 " />
+      <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600  animate-pulse animation-delay-500 " />
+      <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600  animate-pulse animation-delay-1000 " />
     </div>
   );
 };
